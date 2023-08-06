ea3bc1d7d3d8ddc9051d7397e780d1a846d5efc0
izzat <izzat@gmail.com>
2023-08-06 23:03:47 +0500

    made initial layouts and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
-import StyledComponentsRegistry from '../lib/registry'
- 
+import StyledComponentsRegistry from "../lib/registry";
+import { Montserrat } from "next/font/google";
+
+const inter = Montserrat({
+    subsets: ["latin"],
+    display: "swap",
+});
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </body>
-    </html>
-  )
+    return (
+        <html lang='en' className={inter.className}>
+            <body>
+                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+            </body>
+        </html>
+    );
 }
