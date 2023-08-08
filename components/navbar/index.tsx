b6cd6c275fce5d92c395ff73d17cd08093e94d78
izzat <izzat@gmail.com>
2023-08-08 22:36:33 +0500

    added classnames to Navbar links

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,13 +1,13 @@
 "use client";
 import Link from "next/link";
-import { navLinks } from "./data";
 import style from "./style.module.css";
+import { navLinks } from "./data";
 
 export default function Navbar() {
     return (
         <div className={style.nav}>
             {navLinks.map((el) => (
-                <Link href={el.href} key={el.id}>
+                <Link href={el.href} key={el.id} className='nav-links'>
                     {el.text}
                 </Link>
             ))}
