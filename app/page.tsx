55ca41467946de9be1bbf067d12f7d63633adbbc
izzat <izzat@gmail.com>
2023-08-09 20:48:13 +0500

    added metaData to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-"use client";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Khudoyberganov`s blog",
+    description:
+        "Khudoyberganov Izzatning blog sahifasi | Dasturlash | Frontend | Next js",
+};
 export default function Home() {
-    return <div className='container'>hiii</div>;
+    return <div className='container'></div>;
 }
