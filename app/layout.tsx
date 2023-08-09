99445de30cbd1a3e338beba9ed8bd41216a11438
izzat <izzat@gmail.com>
2023-08-09 21:12:10 +0500

    added new metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Quicksand } from "next/font/google";
+import type { Metadata } from "next";
 import Navbar from "@/components/navbar";
 import "@/style/main.css";
 
@@ -8,6 +9,34 @@ const inter = Quicksand({
     weight: ["400", "500", "700"],
     display: "swap",
 });
+
+export const metadata: Metadata = {
+    metadataBase: new URL("https://khudoyberganov.uz"),
+    title: {
+        default: "Khudoyberganov Izzat",
+        template: "%s | Khudoyberganov Izzat",
+    },
+    description: "Developer, writer, and creator.",
+    openGraph: {
+        title: "Khudoyberganov Izzat",
+        description: "Developer, writer, and creator.",
+        url: "https://khudoyberganov.uz",
+        siteName: "Khudoyberganov Izzat",
+        locale: "uz-UZ",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            index: true,
+            follow: true,
+            "max-video-preview": -1,
+            "max-image-preview": "large",
+            "max-snippet": -1,
+        },
+    },
+};
 
 export default function RootLayout({
     children,
